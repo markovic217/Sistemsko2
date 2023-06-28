09507b487f24c967daf11633f73a5628d8a8d8e6
markovic217 <lukamarkovic01@gmail.com>
2023-06-28 21:40:55 +0200

    final

--- DrugiProjekat/Program.cs
+++ DrugiProjekat/Program.cs
@@ -55,6 +55,16 @@ namespace DrugiProjekat
                         Console.WriteLine(url);
                         var response = await client.GetAsync(url);
                         response.EnsureSuccessStatusCode();
+                        Task responseTask = Task.Run(() =>
+                        {
+                            context.Response.StatusCode = (int)HttpStatusCode.OK;
+                            context.Response.ContentType = "text/plain";
+                            string data = $"Status code: {(int)HttpStatusCode.OK}.\n Uspesno poslat zahtev";
+                            byte[] buffer = Encoding.UTF8.GetBytes(data);
+                            context.Response.ContentLength64 = buffer.Length;
+                            context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                            context.Response.Close();
+                        });
                         var content = await response.Content.ReadAsStringAsync();
                         dynamic comments = JsonConvert.DeserializeObject(content);
                         //Console.WriteLine(comments["items"]);
@@ -63,13 +73,7 @@ namespace DrugiProjekat
                             string post = comment["snippet"]["topLevelComment"]["snippet"]["textOriginal"];
                             commentSubject.OnNext(post);
                         }
-                        context.Response.StatusCode = (int)HttpStatusCode.OK;
-                        context.Response.ContentType = "text/plain";
-                        string data = $"Status code: {(int)HttpStatusCode.OK}.\n Uspesno poslat zahtev";
-                        byte[] buffer = Encoding.UTF8.GetBytes(data);
-                        context.Response.ContentLength64 = buffer.Length;
-                        context.Response.OutputStream.Write(buffer, 0, buffer.Length);
-                        context.Response.Close();
+                        
                     }
                     catch (HttpRequestException e)
                     {               
@@ -198,21 +202,3 @@ namespace DrugiProjekat
         }
     }
 }
-
-/*string text = @"It's easy to understand for developers and 'plays well with everything'.
-            Don't underestimate the importance of toolchain support. Want to spin up a Ruby microservice which speaks HTTP?
-            Sinatra and you're done. Go? Whatever the Go HTTP library is and you're done. Need to interact with it from the command line?
-        Curl and you're done. How about from an automated testing script written in Ruby? Net:HTTP/HTTParty and you're done.
-        Thinking about how to deploy it vis-a-vis firewall/etc? Port 80 and you're done. Need coarse-grained access logging?
-        Built into Nginx/etc already; you're done. Uptime monitoring? Expose a /monitor endpoint; provide URL to existing monitoring solution;
-        you're done. Deployment orchestration? Use Capistrano/shell scripts/whatever you already use for the app proper and you're done. 
-        Encrypted transport? HTTPS and you're done. Auth/auth? A few options that you're very well-acquainted with and are known to be 
-        consumable on both ends of the service trivially.
-            Edit to note: I'm assuming, in the above, that one is already sold on the benefits of a microservice 
-        architecture for one's particular app/team and is deciding on transport layer for the architecture. 
-        FWIW, I run ~4 distinct applications, and most of them are comparatively large monolithic Rails apps.
-        My company's bookkeeping runs in the same memory space as bingo card PDF generation.
-            Things that would tilt me more towards microservices include a very rapid deployment pace, 
-        large engineering organizations which multiple distinct teams which each want to be able to control 
-        deploys/architecture decisions, particular hotspots in the application which just don't play well with 
-        one's main technology stack (as apparently happened in the app featured in this article), etc.";*/
